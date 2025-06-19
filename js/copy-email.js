58806a6f2814db1e9e4c387b5bb60f70e7048f0d
Kirill Gringauz <kirill.gringauz@kodex-ai.com>
2025-06-19 23:47:15 +0200

    Copy address

---
+++ js/copy-email.js
@@ -0,0 +1,13 @@
+const emailLink = document.getElementById('copy-email');
+const tooltip = new bootstrap.Tooltip(emailLink, {trigger: 'manual'});
+
+emailLink.addEventListener('click', e => {
+    e.preventDefault();
+    navigator.clipboard
+        .writeText(emailLink.textContent.trim())
+        .then(() => {
+            tooltip.show();
+            setTimeout(() => tooltip.hide(), 1500);
+        })
+        .catch(err => console.error('Copy failed', err));
+});
